8c13d8508e90e010484f9dda36f738949536c6ca
G-jjunny <rudwns9551@naver.com>
2025-05-03 19:53:17 +0900

    dist update

--- dist/src/app.js
+++ dist/src/app.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 require("dotenv/config");
+const auth_1 = __importDefault(require("./routes/auth"));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 4000;
 app.use(express_1.default.json());
@@ -14,6 +15,8 @@ app.get("/ping", (req, res) => {
 app.get("/", (req, res) => {
     res.send("Server is running!");
 });
+// login api
+app.use("/", auth_1.default);
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
