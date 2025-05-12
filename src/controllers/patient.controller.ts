9806027c23c43827486d9f306e88117f68adb8da
G-jjunny <rudwns9551@naver.com>
2025-05-12 14:49:27 +0900

    patient 스키마 수정 및 drop api추가

--- src/controllers/patient.controller.ts
+++ src/controllers/patient.controller.ts
@@ -25,3 +25,19 @@ export const getPatient = async (req: Request, res: Response) => {
     res.status(500).json({ message: "기관 환자 조회 실패", error: err });
   }
 };
+
+export const dropPatients = async (req: Request, res: Response) => {
+  const { ids } = req.body;
+
+  try {
+    await prisma.patientData.updateMany({
+      where: { id: { in: ids } },
+      data: { droped: true },
+    });
+
+    return res.status(200).json({ message: "Saved" });
+  } catch (err) {
+    console.error("Drop error:", err);
+    return res.status(500).json({ message: "서버 오류" });
+  }
+};
