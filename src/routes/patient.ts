2b5f9e163d34823beebd1ecc7d6679ce9f3374f3
G-jjunny <rudwns9551@naver.com>
2025-05-12 19:08:33 +0900

    route추가

--- src/routes/patient.ts
+++ src/routes/patient.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import {
+  downloadPatientsExcel,
   getPatient,
   getPatientAll,
   patchDropPatients,
@@ -11,6 +12,6 @@ const router = Router();
 router.get("/patient", getPatientAll);
 router.get("/patient/:institution", getPatient);
 router.patch("/patient/drop", patchDropPatients);
-router.get("/patient/download");
+router.get("/patient/download", downloadPatientsExcel);
 
 export default router;
